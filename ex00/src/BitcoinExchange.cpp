6a9e262476c1fbd2a88b194516d99a674b100004
Berke Ibrahim Ince <bince@bess-f2r1s5.clusters.42paris.fr>
2025-03-04 13:50:44 +0100

    firs ex is done, need more tests

--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -54,10 +54,34 @@ void BitcoinExchange::fillDateAndValue()
         if (date != NULL && value_str != NULL)
             dateAndValue[std::string(date)] = strtofloat(value_str);
     }
-    //printData();
 }
 
+bool isDateCorrect(std::string date)
+{
+    char *year = strtok((char *)date.c_str(), "-");
+    char *month = strtok(NULL, "-");
+    char *rest = strtok(NULL, "-");
+    char *day = strtok(rest, ",");
 
+    if (!year || !month || !rest || !day)
+        return false;
+    float f_month = strtofloat(month);
+    if (f_month < 0 || f_month > 12)
+        return false;
+    if ((f_month == 1 || f_month == 3 || f_month == 5 || f_month == 7 || f_month == 7|| f_month == 8 || f_month == 10 || f_month == 12) && strtofloat(day) > 31)
+        return false;
+    else if (f_month != 2 && strtofloat(day) > 30)
+        return false;
+    else
+    {
+        float f_year = strtofloat(year);
+        if ((((int)f_year % 4) == 0) && strtofloat(day) > 29)
+            return false;
+        else if(strtofloat(day) > 28)
+            return false;
+    }
+    return true;
+}
 
 void BitcoinExchange::startConversion(std::string input_file)
 {
@@ -80,8 +104,16 @@ void BitcoinExchange::startConversion(std::string input_file)
             std::cerr << "Error: Not a positive number." << std::endl;
             continue;
         }
+        if (!isDateCorrect(date))
+        {
+            std::cerr << "Date is wrong." << std::endl;
+            continue;
+        }
         float valueByDate = getValueByDate(date);
-        std::cout << date << " => " << amount_str << " = " << valueByDate * amount << std::endl;
+        if (valueByDate < 0)
+            std::cerr << "Date cannot be older than the oldest date on the data sheet." << std::endl;
+        else
+            std::cout << date << " => " << amount_str << " = " << valueByDate * amount << std::endl;
     }
 }
 
@@ -89,6 +121,8 @@ void BitcoinExchange::startConversion(std::string input_file)
 float searchClosestDate(std::map<std::string, float>& dataMap, std::string date)
 {
     std::map<std::string, float>::iterator it = dataMap.begin();
+    if (it->first.compare(date) > 0)
+        return -1;
     for (; it != dataMap.end(); it++)
     {
         int diff = it->first.compare(date);
