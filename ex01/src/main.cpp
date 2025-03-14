df274d87ab6908716395ccae1d6264ab0514712e
Berke Ibrahim Ince <bince@bess-f2r4s16.clusters.42paris.fr>
2025-03-14 16:11:29 +0100

    some tests are made

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,13 +6,18 @@
 /*   By: bince <bince@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 15:53:36 by bince             #+#    #+#             */
-/*   Updated: 2025/03/14 15:53:37 by bince            ###   ########.fr       */
+/*   Updated: 2025/03/14 16:11:13 by bince            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
 #include "RPN.hpp"
+
+// 5 1 2 + 4 * + 3 -                    = 14
+// 7 2 8 * + 3 -                        = 20
+// 9 8 + 7 6 + * 5 4 + 3 2 + * -        = 176
+// 9 8 + 7 6 + * 5 4 + * 3 2 + * 1 -    = 9944
+// 9 8 + 7 6 + * 5 4 + * 3 2 + * 1 - 0 / = Invalid
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         std::cerr << "Error: Invalid arguments" << std::endl;
